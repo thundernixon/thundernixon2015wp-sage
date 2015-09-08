0749c9e91970938e945d53b81c2f6a38dadc7cc1
STEPHEN NIXON <snixon@us.ibm.com>
2015-09-07 23:16:39 -0400

    changed work, play, blog to projects, lettering, and weblog

--- templates/navbar.php
+++ templates/navbar.php
@@ -7,19 +7,19 @@
 		<nav class="main-nav">
 
 			<ul class="container">
-				<a data-tab="work"   href="#work"  class="" id="tab-1">
+				<a data-tab="work"   href="#projects"  class="" id="tab-1">
 					<li>
-						<span>Work</span>
+						<span>Projects</span>
 					</li>
 				</a>
-				<a data-tab="instagram"  href="#play" class="" id="tab-2">
+				<a data-tab="instagram"  href="#lettering" class="" id="tab-2">
 					<li>
-					 <span>Play</span> 	<!--#play -->
+					 <span>Lettering</span> 	<!--#play -->
 					</li>
 				</a>
-				<a data-tab="blog"  href="#blog"  class="" id="tab-3">
+				<a data-tab="blog"  href="#weblog"  class="" id="tab-3">
 					<li>
-						<span>Blog</span>
+						<span>Weblog</span>
 					</li>
 				</a>
 			</ul>
@@ -29,29 +29,29 @@
 	<nav class="main-nav">
 
 			<ul class="container">
-				<a data-tab="work"   href="<?php bloginfo('url'); ?>#work"  class="" id="tab-1">
+				<a data-tab="work"   href="<?php bloginfo('url'); ?>#projects"  class="" id="tab-1">
 					<li>
-						<span>Work</span>
+						<span>Projects</span>
 					</li>
 				</a>
-				<a data-tab="instagram"  href="<?php bloginfo('url'); ?>/#play" class="" id="tab-2">
+				<a data-tab="instagram"  href="<?php bloginfo('url'); ?>/#lettering" class="" id="tab-2">
 					<li>
-						<span>Play</span>
+						<span>Lettering</span>
 					</li>
 				</a>
-				<a data-tab="blog"  href="<?php bloginfo('url'); ?>/#blog"  class="" id="tab-3">
+				<a data-tab="blog"  href="<?php bloginfo('url'); ?>/#weblog"  class="" id="tab-3">
 					<li>
-						<span>Blog</span>
+						<span>Weblog</span>
 					</li>
 				</a>
 			</ul>
+<!---test comment---->
 
 	</nav>
 
-
-<?php endif; ?>
-
-
-	<!--You should clean up the js here, and move the onclicks to event listeners and data attributes in the actual javascript documents-->
+  <!--You should clean up the js here, and move the onclicks to event listeners and data attributes in the actual javascript documents-->
 
 	<!--You should consider putting the showSection and growTab functions into a single function, and you can just grab it by a data attibrute, potentially in an if/then statement -->
+
+
+<?php endif; ?>
