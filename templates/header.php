5321ef9b495d92acf7f0c7c83e26ebad2333df7c
STEPHEN NIXON <snixon@us.ibm.com>
2016-05-15 22:39:59 -0400

    trying to fix rendering of name on other computers

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,6 @@
               <span id="logo"><img src="<?php bloginfo('template_directory'); ?>/dist/images/stn-logo-3.svg"></span>
               <span class="brand">Stephen Nixon</span>
         </a>
-    <!-- <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a> -->
 
     <a class="about-modal-link small-caps" type="button" data-toggle="modal" data-target="#myModal" href=#>About</a>
 
